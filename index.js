714c1c47e1b97e015d7b31ebdec293731218fd24
Ahmet Tunç Aras <74108753+Latth@users.noreply.github.com>
2025-01-04 14:28:23 +0300

    first commit
    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(ecxpress.json());
+
+// Check API
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API Working', status: true,});
+});
+
+
+app.post('/api/v1/check-access', (req, res) => {
+    const data = req.body;
+
+    
+});
