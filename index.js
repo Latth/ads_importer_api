cdb7e129c0fa3c114da78b0078ade2b91d69f894
Ahmet aras <latherield@yandex.com>
2025-01-04 16:04:09 +0300

    updates for the express and mysql

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(ecxpress.json());
+app.use(express.json());
+const db = mysql.createConnection({
+    host: 'alm-ads-importer-api-db-dropwapi.g.aivencloud.com',
+    port: 17621,
+    user: "avnadmin",
+    password: "AVNS_LpvDV0xUtwY6IvQ7ICf",
+    database: 'defaultdb',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log('Couldnt connect to database '+err);
+    } else {
+        console.log('Connected to database');
+    }
+});
 
 // Check API
-
 app.get('/', (req, res) => {
   res.json({ message: 'API Working', status: true,});
 });
 
 
-app.post('/api/v1/check-access', (req, res) => {
+app.post('/api/check-access', (req, res) => {
     const data = req.body;
-
-    
 });
